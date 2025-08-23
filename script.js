88976dd4b9bbe91eaf82d7d143b31a3c7423af8b
Agaporni22 <00001130287440SP@al.educacao.sp.gov.br>
2025-08-23 15:45:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-    const alternaContraste = document.getElementById('alterna-contraste')
-
+    
     botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+    botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
 })
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
  })
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
